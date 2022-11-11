6d978f77c2cb98ccd953ce998c8473cf3e222d13
JK1117 <41583468+JK-1117@users.noreply.github.com>
2022-11-11 18:58:59 +0800

    Delete Environment.js

--- common/constants/Environment.js
+++
@@ -1,8 +0,0 @@
-export default {
-  firebaseConfig: {
-    apiKey: "AIzaSyCZhL1HHy5yroIIpZYIMRxwMbCy2e2tzpQ",
-    authDomain: "dragonfly-bms.firebaseapp.com",
-    databaseURL: "https://dragonfly-bms.firebaseio.com/",
-    storageBucket: "gs://dragonfly-bms.appspot.com"
-  }
-};
